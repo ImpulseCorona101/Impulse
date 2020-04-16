fa105b7d622e624b19c8229e5cae7d36c2ac2d97
Bhabalomkar421 <Omkarbhabal11@gmail.com>
2020-04-16 18:15:48 +0530

    farmer homepage done

--- AgroCraft/Includes/db.php
+++ AgroCraft/Includes/db.php
@@ -1,6 +1,6 @@
 <?php 
 
-        $con = mysqli_connect("localhost","root","","agrocraft");
+        $con = mysqli_connect("localhost","root","","impulse");
 
         if (mysqli_connect_errno()) {
                 echo "Failed to connect to MySql " . mysqli_connect_error();
