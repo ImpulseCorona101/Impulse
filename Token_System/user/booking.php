5080b7ea950686bcedaae0a22328dae2312161ab
azim2429 <abdulazim0402@gmail.com>
2020-04-16 21:32:22 +0530

    krdia

--- Token_System/user/booking.php
+++ Token_System/user/booking.php
@@ -12,7 +12,7 @@
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
@@ -553,6 +553,24 @@
                                                 }
                                                 </script>
                                             </div>
+
+
+                                            <?php
+                                            include("db.php")
+                                            if (isset($_GET['search'])){
+                                               $search_query = $_GET['search'];
+                                               $get_shop = "select shopName,shopAddress,phoneenumber1 from sign_in where pincode like '%$search_query%'";
+                                               $run_shop = mysqli_query($con, $get_shop);
+                                               $count = mysqli_num_rows($run_shop);
+                                               if ($count > 0) {
+                                               echo "<br>";
+                                               while ($rows = mysqli_fetch_array($run_pro)) {
+                                                    $shopName = $rows['shopName'];
+                                                    $shopAddress = $rows['shopAddress'];
+                                                    $phoneenumber1 = $rows['phoneenumber1'];
+                                                    }
+                                                    
+                                                    ?>
 
 </body>
 </html>
